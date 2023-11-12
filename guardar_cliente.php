b5f10e2be2b1a42a5c69b295a7fb642d124fe8d6
shey83764 <133888508+shey83764@users.noreply.github.com>
2023-11-12 11:43:56 -0300

    Update guardar_cliente.php

--- guardar_cliente.php
+++ guardar_cliente.php
@@ -5,11 +5,6 @@ $loca=$_GET['localidad'];
 echo $nom.$dire.$loca;
 $SQL4="INSERT INTO cliente (nombre_cliente,direccion,localidad)VALUES('$nom','$dire',$loca)";
 include('conexion.php');
-$resul4=$cone->query($SQL4);
-if ($resul4) {
-    echo "<h2>El registro se guardó correctamente</h2>";
-        }
-    else {
-        echo "<h2>No se pudo guardar el registro</h2>";
-    }
+$cone->query($SQL4);
+header('location:mostrarcliente.php');
 ?>
