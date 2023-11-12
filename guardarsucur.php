517ddf304627bc77260da4e7ace844e48ded4996
shey83764 <133888508+shey83764@users.noreply.github.com>
2023-11-12 11:41:12 -0300

    Update guardarsucur.php

--- guardarsucur.php
+++ guardarsucur.php
@@ -4,11 +4,6 @@ $lc=$_GET['localidad'];
 echo$nm.$lc;
 $SQL5="INSERT INTO sucursales (nombre,localidad)VALUES('$nm',$lc)";
 include('conexion.php');
-$resultado=$cone->query($SQL5);
-if ($resultado) {
-    echo "<h2>El registro se guardó correctamente</h2>";
-        }
-    else {
-        echo "<h2>No se pudo guardar el registro</h2>";
-    }
+$cone->query($SQL5);
+header('location: mostrarsucur.php');
 ?>
